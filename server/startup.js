6a0942b0290893172d5a84be70acb71c0f4ef23f
Martin Steinmann <martin@caliviaconsult.com>
2018-05-30 17:16:36 -0400

    allow admin account and project creation via settings.json file

--- server/startup.js
+++ server/startup.js
@@ -1,10 +1,35 @@
 Meteor.startup(() => {
-  if (!Meteor.users.findOne({ username: 'admin' })) {
+  const adminUser = Meteor.settings.private.adminUser;
+  if (!Meteor.users.findOne({ username: adminUser.username })) {
     Accounts.createUser({
-      username: 'admin',
-      email: 'admin@admin.com',
-      password: 'admin',
+      username: adminUser.username,
+      email: adminUser.email,
+      password: adminUser.password,
       plan: 'business'
     });
+  }
+
+  // allow creating apps in the config.json file
+  const apps = Meteor.settings.private.apps;
+  if (apps.length > -1) {
+    apps.map((app) => {
+      if (app.name && app.appId && app.secret) {
+        if (!Apps.findOne(app.appId)) {
+          Apps.insert({
+            _id: app.appId,
+            name: app.name,
+            created: new Date(),
+            owner: Meteor.users.findOne({
+              username: { $eq: adminUser.username }})._id,
+            secret: app.secret,
+            plan: 'business',
+            subShard: Math.floor(Math.random() * 128),
+          })
+          console.log(`App ${app.name} created`);
+        } else {
+          console.log('Duplicate app, ignored');
+        }
+      }
+    })
   }
 });
